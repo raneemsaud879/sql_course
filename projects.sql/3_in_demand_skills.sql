8e1810dc632e7cbd0300d0256c6a620a94e123ca
raneemsaud879 <rraaneem7@gmail.com>
2026-07-24 10:41:14 +0300

    ex.3

--- projects.sql/3_in_demand_skills.sql
+++ projects.sql/3_in_demand_skills.sql
@@ -5,6 +5,7 @@ count(skills_job_dim.job_id) as demand_count
 from job_postings_fact
 INNER JOIN skills_job_dim on job_postings_fact.job_id=skills_job_dim.job_id
 INNER join skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
+where job_title_short='Data Analyst'
 group by skills
 order by demand_count
 limit 5
