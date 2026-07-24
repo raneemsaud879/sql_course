072736095e5ae68f63cc854e1df34f2d77753241
raneemsaud879 <rraaneem7@gmail.com>
2026-07-24 10:36:50 +0300

    ex.3

---
+++ projects.sql/3_in_demand_skills.sql
@@ -0,0 +1,10 @@
+/*in demand skills*/
+SELECT
+skills,
+count(skills_job_dim.job_id) as demand_count
+from job_postings_fact
+INNER JOIN skills_job_dim on job_postings_fact.job_id=skills_job_dim.job_id
+INNER join skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
+group by skills
+order by demand_count
+limit 5
